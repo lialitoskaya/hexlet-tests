c6bb8971384851a37831c2ce053409c2f3970488
liya <liyarheon@gmail.com>
2023-03-12 18:52:58 +0300

    add capitalize function to src

---
+++ src/capitalize.js
@@ -0,0 +1,7 @@
+const capitalize = (text) => {
+  const firstChar = text[0].toUpperCase();
+  const restSubstring = text.slice(1);
+  return `${firstChar}${restSubstring}`;
+};
+
+export default capitalize
